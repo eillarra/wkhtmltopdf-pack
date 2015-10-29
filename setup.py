8f87d2c0e9e5aa7a2b128b3adc93c84c43e60cf4
Eneko Illarramendi <eneko@illarra.com>
2015-10-29 19:56:26 +0100

    wkhtmltopdf 0.12.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wkhtmltopdf-pack',
-    version='0.12.2.1',
+    version='0.12.2.4',
     url='https://github.com/illarra/wkhtmltopdf-pack',
     author='illarra',
     author_email='team@illarra.com',
